de3ef908a1b1b410460ec26f83600b720ef46ff3
StephanRaab <sarcartist@gmail.com>
2015-09-27 15:45:47 -0400

    Fixes
    • Added enter key commit
    • Got deletion to work with event.target
    • Maintained button click
    • Added css positioning

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
 
-    $("button").click(function(){
+
+//using enter key
+  $('#chatTest').keypress(function (e) {
+   var key = e.which;
+   if(key == 13)  // the enter key code
+    {
         var chatName = $("#chatTest").val();
-        $('#test').show();
+        $("#test").show();
         $('#chatInput').html(chatName);
-        $('#pastIdeas').append('<li>' + chatName +'<br>');
-    });
+        $('#pastIdeas').append('<li>' + chatName + '<br>');
+      return false;
+    }
+  });
 
-  //  var closeButton = <i class="fa fa-times"></i>
+//if user clicks on button with moouse
+  $("button").click(function() {
+    var chatName = $("#chatTest").val();
+    $('#test').show();
+    $('#chatInput').html(chatName);
+    $('#pastIdeas').append('<li>' + chatName +'<br>');
+  });
 
-/*    $(closeButton).click(function(){
-      $('<li>').toggleClass("fade");
-    });
-*/    
+//delete past list items
+  $( '#pastIdeas' ).click( 'li' , function(event){
+    $(event.target).hide(250);
+  });
+
 });
